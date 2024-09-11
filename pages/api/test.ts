5533c812e017226b182fad14ad804b6c4d433364
Dany <deng1314159@gmail.com>
2024-09-11 10:03:17 +0000

    api  update

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,8 +1,10 @@
 export default function handler(req, res) {
+  console.log(`call ----api---${new Date().getTime()}`);
+
   // 检查请求方法
   if (req.method === "GET") {
     // 处理 GET 请求
-    console.log(`call ----api---${new Date().getTime()}`)
+    console.log(`call ----api---from--get--${new Date().getTime()}`);
     res.status(200).json({ message: "Hello from /api/test" });
   } else {
     // 处理其他请求
