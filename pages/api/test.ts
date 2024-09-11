2b44b943e075968cac05c3088250e39349d245b7
Dany <deng1314159@gmail.com>
2024-09-11 09:55:29 +0000

    add test api

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -2,6 +2,7 @@ export default function handler(req, res) {
   // 检查请求方法
   if (req.method === "GET") {
     // 处理 GET 请求
+    console.log(`call ----api---${new Date().getTime()}`)
     res.status(200).json({ message: "Hello from /api/test" });
   } else {
     // 处理其他请求
